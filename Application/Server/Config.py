caced55fc999b7402e90798e982ae5cafbbe1fdc
Sehnsucht <Credgate1992@hotmail.com>
2021-11-17 12:31:33 -0500

    added keys

--- Application/Server/Config.py
+++ Application/Server/Config.py
@@ -5,3 +5,4 @@ PORT = 10023           #The port to listen to connections on
 ###Certificate Generation
 HOST_NAME = 'localhost'   #The DNS name of your server. If you do not use one leave it as localhost
 CERT_IP = ['10.0.0.64']     #Setting to my server machines local ip address... If you're going over the internet use your public IP Keep this as a list.
+PASSWORD = b"password"
