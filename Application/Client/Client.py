efbcdd09dc929fe66de5e2a9e5464d45db3fef80
Sehnsucht <Credgate1992@hotmail.com>
2021-11-18 14:14:38 -0500

    Added/restrcutured commands. Client auto connects.

--- Application/Client/Client.py
+++ Application/Client/Client.py
@@ -1,51 +1,67 @@
 from random import randrange
 import ssl, socket, threading
+from Device import Device
 from DataFormatter import Protocol_Receive, Protocol_Send
-from CommandHelper import Client_Command
+from ServerHelper import Client_Command
+from CommandHandler import Terminal_Command
 import Config
 
-
-###Connects to the host on the given port
 def Connect(host, port):
-    conn = context.wrap_socket(socket.socket(socket.AF_INET),    #Wrap the socket with TLS
-                                        server_hostname=host)
+    context = ssl.create_default_context()                                    #helps create SSLContext objects for common purposes
+    context.load_verify_locations('./Auth/certificate.pem')                   #Load the cert so it will be accepted since it is self signed
 
-    conn.connect((host, port))
-    # cert = conn.getpeercert()                                     #save the cert as cert variable
+    connection = context.wrap_socket(socket.socket(socket.AF_INET),           #Wrap the socket with TLS
+                                    server_hostname=host)
+
+    connection.connect((host, port))
 
     print(f"Connected to {host}:{port}")
 
-    Protocol_Send(conn, str(randrange(0,10000)))            #Send the device name TEMPORARILY setting to random number
-    Protocol_Send(conn, "DeviceArchetype")                         #Sending the device type
+    Protocol_Send(connection, device.name)                                      #Send the device name TEMPORARILY setting to random number
+    Protocol_Send(connection, device.archetype)                                            #Sending the device type
+    receive_thread = threading.Thread(target=Receive_Data, args=(connection,))   #Starting thread to recieve data
+    receive_thread.start()
 
-    return conn                                                    #Returns the connection object
+    return connection                                                                       #Returns the connection object
+
 
 ###Receives data from the given connection
 def Receive_Data(connection):
-    while True:
+    global connected
+    while connected:
         try:
             message = Protocol_Receive(connection)
             Client_Command(connection, message)
         except:                                             
             print("Connection to host lost.")
-            connection.close()
-            break
+            connected = False
+
 
 ###Sends data to the given connection
-def Send_Data(connection):                                      
-    while True:
-        message = input("")
-        Protocol_Send(connection, message)                    
+def Send_Data(connection, message):                               
+    Protocol_Send(connection, message)
 
+def Terminal():
+    while True:                                                                                                                                                      
+        try:
+            Terminal_Command()
+        except Exception as e:
+            print(e)
 
+    
 if __name__ == "__main__":
-    context = ssl.create_default_context()                                    #helps create SSLContext objects for common purposes
-    context.load_verify_locations('./Auth/certificate.pem')                   #Load the cert so it will be accepted since it is self signed
-
-    connection = Connect(Config.SERVER_IP, Config.SERVER_PORT)
-
-    receive_thread = threading.Thread(target=Receive_Data, args=(connection,))   #Starting thread to recieve data
-    receive_thread.start()
-
-    send_thread = threading.Thread(target=Send_Data, args=(connection,))         #Starting thread to send data
-    send_thread.start()
+    name = input("Name your device: ").lower()
+    archetype = input("Give your device a type: ").lower()
+    device = Device(name, archetype)
+    connected = False
+    terminal_thread = threading.Thread(target=Terminal)
+    terminal_thread.start()
+    while True:
+        if connected == False:
+            try:
+                connected = True
+                connection = Connect(Config.SERVER_IP, Config.SERVER_PORT)
+                device.server = connection
+            except:
+                connected = False
+                print("Failed to connect. Trying again...")
--- Application/Server/Commands.py
+++ Application/Server/Commands.py
@@ -2,83 +2,52 @@ from Device import Device, Group
 from DataFormatter import Protocol_Receive, Protocol_Send
 
 ################## Commands called from a client ############################
-def Set_Name(client_device, split_data):
-    name = split_data[2]
-    if name in Device.devices.keys():
-        Protocol_Send(client_device.client, "Error: That name is already in use.")
+def Set_Name(device, new_name):
+    if new_name in Device.devices.keys():
+        Protocol_Send(device.client, "Error: That name is already in use.")
         print("Error: Device name is already in use.")
-    else:
-        new_name = name                                              
-        print(f"{client_device.name}'s name has been changed to {new_name}")
-        client_device.Change_Name(new_name)
-def Set_Type(client_device, split_data):
-    new_type = split_data[2]                                               
+    else:                                            
+        print(f"{device.name}'s name has been changed to {new_name}")
+        device.Change_Name(new_name)
+def Set_Type(client_device, new_type):                       
     print(f"{client_device.name}'s device type changed to {new_type}")
     client_device.archetype = new_type
 
 ################## Commands called from server ############################
-def Send(split_data):
-    try:
-        Protocol_Send(Device.devices[split_data[1]].client, " ".join(split_data[2:]))
-        print(f"Message successfully sent to {Device.devices[split_data[1]].name}")
-    except:
-        print("Error: Client doesn't exist by that name. Or you're trying to send unsuported data values.")
+def Send(device, message):
+    Protocol_Send(device.client, message)
+    print(f"Message successfully sent to {device.name}")
 def List():
-    [print(f"Name:{Device.devices[i].name} Type:{Device.devices[i].archetype}") for i in Device.devices.keys()]
+    [print(f"ID:{i} Name:{Device.devices[i].name} Type:{Device.devices[i].archetype}") for i in Device.devices.keys()]
 
-def Run(split_data):
-    try:
-        formatted = " ".join(split_data[0:1] + split_data[2:])
-        print(formatted)
-        Protocol_Send(Device.devices[split_data[1]].client,formatted)
-    except:
-        print("Error: Client doesn't exist by that name. Or you're trying to send unsuported data values.")
-def Group_Create(split_data):
-    group_name = split_data[2]
+def Run(device, run_command):
+    print(run_command)
+    Protocol_Send(device.client,run_command)
+def Group_Create(group_name): 
     if group_name in Group.groups.keys():
         print(f"Group {group_name} already exists.")
     else:
         Group(group_name)
         print(f"Group {group_name} created")
-def Group_Add(split_data):
-    group_name = split_data[2]
-    device_name = split_data[3]
-    try:
-        group = Group.groups[group_name]
-        device = Device.devices[device_name]
-        if device in group.devices:
-            print("Device already exists in that group.")
-        else:
-            group.Add_Device(device)
-            device.group = group
-            print(f"Added {device_name} to {group_name}")
-    except Exception as e:
-        print(e)
+def Group_Add(group, device):
+    if device in group.devices:
+        print("Device already exists in that group.")
+    else:
+        group.Add_Device(device)
+        device.group = group
+        print(f"Added {device.name} to {group.name}")
 def Group_List():
     print(Group.groups)
-def Group_Delete(split_data):
-    try:
-        group_name = split_data[2]
+def Group_Delete(group_name):
         Group.Group_Delete(group_name)
         print(f"{group_name} deleted")
-    except Exception as e:
-        print(e)
-def Group_Remove(split_data):
-    try:
-        group_name = split_data[2]
-        group = Group.groups[group_name]
-        device_name = split_data[3]
-        device = Device.devices[device_name]
-        group.Remove_Device(device)
-        print(f"{device_name} removed from {group_name}")
-    except Exception as e:
-        print(e)
-def Group_Send(split_data):
-    try:
-        group_name = split_data[2]
-        group = Group.groups[group_name]
-        message = " ".join(split_data[3:])
-        for device in group.devices:
-            Protocol_Send(device.client, message)
-    except Exception as e:
-        print(e)
+def Group_Remove(group, device):
+    group.Remove_Device(device)
+    print(f"{device.name} removed from {group.name}")
+
+def Group_Send(group, message):
+    for device in group.devices:
+        Protocol_Send(device.client, message)
+def Group_Run(group, run_command):
+    for device in group.devices:
+        Protocol_Send(device.client,run_command)
